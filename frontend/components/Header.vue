8895b3ad6d6013c9524d3b14feb46d0307e6be67
Krzysztof <halidu13@gmail.com>
2024-03-27 12:57:40 +0100

    fix: add link in header

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -4,7 +4,7 @@ const { locale, setLocale } = useI18n();
 
 <template>
   <header class="flex justify-between items-center py-4 w-full">
-    <span class="text-xl font-bold">Blog</span>
+    <nuxt-link to="/" class="text-xl font-bold">Blog</nuxt-link>
     <button
       @click="setLocale(locale === 'en' ? 'pl' : 'en')"
       class="p-2 hover:bg-gray-200"
